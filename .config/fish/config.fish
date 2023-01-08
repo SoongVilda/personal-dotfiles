05370824681dae680090c9f90b27ee196f587999
Soong-Vilda <61845673+SoongVilda@users.noreply.github.com>
2023-01-08 10:33:38 +0100

    Create config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+## Set values
+# Hide welcome message
+set fish_greeting
+set VIRTUAL_ENV_DISABLE_PROMPT "1"
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
